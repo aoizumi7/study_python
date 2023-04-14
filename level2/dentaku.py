7c62d606422c9bc70149b6d9a3a538619e95c78f
aoizumi7 <nezuhitsu7shimakuma@gmail.com>
2023-04-14 20:25:26 +0900

    最新版

--- level2/dentaku.py
+++ level2/dentaku.py
@@ -1,6 +1,10 @@
 import sys
 
-def calculate(operator, num1, num2):
+def calculate(num1, operator, num2):
+    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+       print("数値以外が入力されました")
+       sys.exit(1)
+
     if operator == '+':
         result = num1 + num2
     elif operator == '-':
@@ -8,20 +12,38 @@ def calculate(operator, num1, num2):
     elif operator == '*':
         result = num1 * num2
     elif operator == '/':
-        result = num1 / num2
+       if num2 ==0:
+           print("oで割ることはできません")
+           sys.exit(1)
+       result = num1 / num2 
+    
     else:
         print('無効な演算子です。')
-        return
+        sys.exit(1)
 
-    print('計算結果は', result, 'です。')
+    print("Result: ", result)
 
-if __name__ == '__main__':
-    print(sys.argv[0]) 
+if __name__ == '__main__': 
     if len(sys.argv) != 4:
         print('引数の数が正しくありません。')
-        sys.exit()
+        sys.exit(1)
 
-    operator = sys.argv[1]
-    num1 = int(sys.argv[2])
-    num2 = int(sys.argv[3])
-    calculate(operator, num1, num2)
+    num1 = None
+    try:
+         num1 = float(sys.argv[1])
+    except ValueError:
+         print("Error: 数値以外が入力されました。")
+         sys.exit(1)
+                 
+    operator = sys.argv[2]
+
+    num2 = None
+    try:
+         num2 = float(sys.argv[3])
+    except ValueError:
+         print("Error: 数値以外が入力されました。")
+         sys.exit(1)
+
+    calculate(num1, operator, num2)
+
+
