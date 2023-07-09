e0b75a6e93617ed2f453c197def6c997c2f4f1d8
aoizumi7 <nezuhitsu7shimakuma@gmail.com>
2023-07-09 19:13:09 +0900

    数て当てver.2

--- level4/kazuate.py
+++ level4/kazuate.py
@@ -4,11 +4,15 @@ print(name + "さん、今から数当てゲームをおこないます")
 import random
 target_number = random.randint(1,100)
 
-while True:
-  kazuate = int(input("数字を入力してください:"))
-  #if kazuate <0  or kazuate >100:
-  #print("1以上100以下の数字を入力してください")
+kazuate = int(input("数字を入力してください:"))
+def is_digit(str):
+  for i in range(len(str)):
+    continue
+  else:
+    return False
+  return True
 
+while True:
   if kazuate < target_number:
     print(str(kazuate) + "はプログラムの数字より小さいです")
   if kazuate > target_number:
